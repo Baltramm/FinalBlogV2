0dbdfa91d77b572ad92451d3e29236d3ac3e1a86
Никита Бочковский <nikita.bochkovsky@yandex.ru>
2023-08-05 01:38:40 +0600

    Третий этап проекта, настроена валидация, добавил отдельное решение для сервисов и немного поменяна структура проекта

--- FinalBlog.Data/DBModels/Users/User.cs
+++ FinalBlog.Data/DBModels/Users/User.cs
@@ -19,7 +19,7 @@ namespace FinalBlog.Data.DBModels.Users
 
         public User()
         {
-            Photo = "https://media.istockphoto.com/id/1277275885/ru/%D0%B2%D0%B5%D0%BA%D1%82%D0%BE%D1%80%D0%BD%D0%B0%D1%8F/%D0%B8%D0%BD%D1%82%D0%B5%D1%80%D0%BD%D0%B5%D1%82-%D1%80%D0%B0%D0%B1%D0%BE%D1%87%D0%B8%D1%85-%D0%BC%D0%B5%D1%81%D1%82-%D0%B7%D0%BD%D0%B0%D1%87%D0%BE%D0%BA-%D0%B2%D0%B5%D0%BA%D1%82%D0%BE%D1%80-%D1%87%D0%B5%D0%BB%D0%BE%D0%B2%D0%B5%D0%BA%D0%B0-%D1%81-%D0%BD%D0%BE%D1%83%D1%82%D0%B1%D1%83%D0%BA%D0%BE%D0%BC-%D0%BC%D1%83%D0%B6%D1%81%D0%BA%D0%BE%D0%B9-%D0%BF%D0%BE%D0%BB%D1%8C%D0%B7%D0%BE%D0%B2%D0%B0%D1%82%D0%B5%D0%BB%D1%8C.jpg?s=170667a&w=0&k=20&c=bNrfbuCCyqnW7tsaIe2tzoQIcEB9_FlpiYl_YiGrrGM=";            
+            Photo = "https://gamerwall.pro/uploads/posts/2021-11/thumbs/1637156211_50-gamerwall-pro-p-svetlo-seraya-tekstura-oboi-na-zastavku-50.png";
         }
     }
 }
